f18e4767f8ad417983af95bdf29701aea25eaa17
Dans182 <dgaiteiro@gmail.com>
2022-03-30 15:16:22 +0000

    navBar2

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import NavBar2 from "../component/NavBar2.jsx";
 
 //create your first component
 const Home = () => {
-	return <div></div>;
+	return (
+		<div>
+			<NavBar2 />
+		</div>
+	);
 };
 
 export default Home;
